e0ddcf9b43c0f894792807e3df95e54230271829
Павел Гущин <pavel.gushchin.2005@gmail.com>
2026-04-16 13:26:12 +0300

    Добавьте файлы проекта.

---
+++ TestTasks/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Text;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace TestTasks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
