1b68a10c768ad1edc29f7f81c3699569f59411e0
Павел Гущин <pavel.gushchin.2005@gmail.com>
2026-04-16 13:44:52 +0300

    Implemented Compress function in StringCompressor class

--- TestTasks/Task1_StringCompressor.cs
+++ TestTasks/Task1_StringCompressor.cs
@@ -8,6 +8,44 @@ namespace TestTasks
 {
     internal class Task1_StringCompressor
     {
+        public static string Compress(string baseStr)
+        {
+            if (string.IsNullOrEmpty(baseStr))
+            {
+                throw new ArgumentException($"Argument {nameof(baseStr)} is Null or empty");
+            }
+
+            int count = 1;
+            char ch = baseStr[0];
+            StringBuilder compressedStr = new StringBuilder();
+
+            for (int i = 1; i < baseStr.Length; i++)
+            {
+                if (baseStr[i] == ch)
+                {
+                    count += 1;
+                }
+                else
+                {
+                    compressedStr.Append(ch);
+                    if (count > 1)
+                    {
+                        compressedStr.Append(count);
+                    }
+
+                    ch = baseStr[i];
+                    count = 1;
+                }
+            }
+
+            compressedStr.Append(ch);
+            if (count > 1)
+            {
+                compressedStr.Append(count);
+            }
+
+            return compressedStr.ToString();
+        }
 
     }
 }
